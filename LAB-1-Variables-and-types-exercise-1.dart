d418704ea1c4877d955e8e828ba322c5ede5da72
Naol Kumela <j45674190@gmail.com>
2024-03-08 17:37:34 +0300

    added LAB-1-Variables-and-types-exercise-1.dart

---
+++ LAB-1-Variables-and-types-exercise-1.dart
@@ -0,0 +1,14 @@
+/**
+ * Exercise 1: Create variables to store your name, age, and favorite color, and
+ *  print them to the console using string interpolation
+ */
+
+void main() {
+  String name = "Abebe Kebede";
+  int age = 47;
+  String favoriteColor = "Blue";
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Favorite Color: $favoriteColor');
+}
