a28df57ee9f0eca6240e6247f34b4ddb56d281a5
Naol Kumela <j45674190@gmail.com>
2024-05-21 17:06:15 +0300

    updated some import statements

--- Compose/JetpackCounter/app/src/main/java/com/example/jetpackcounter/MainActivity.kt
+++ Compose/JetpackCounter/app/src/main/java/com/example/jetpackcounter/MainActivity.kt
@@ -1,9 +1,20 @@
 package com.example.jetpackcounter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     var count by mutableStateOf(0) // Initialize count to 0
@@ -11,7 +22,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Counter(count, onIncrement = { count++ }, onDecrement = { count-- }, onReset = { count = 0 })
+            Counter(
+                count = count,
+                onIncrement = { count++ },
+                onDecrement = { count-- },
+                onReset = { count = 0 }
+            )
         }
     }
 
@@ -38,6 +54,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-
 }
-
